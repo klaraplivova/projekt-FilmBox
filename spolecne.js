e8b27cb30c21f720b6c05766c3367ca082a30d8a
klaraplivova <126472015+klaraplivova@users.noreply.github.com>
2024-06-13 22:40:19 +0200

    assignment 3 finalised

---
+++ spolecne.js
@@ -0,0 +1,17 @@
+// ukol 3
+const tlacitko = document.querySelector("#menu-tlacitko")
+
+const rozbalit = () => {
+    const polozky = document.querySelector('#menu-polozky')
+    polozky.classList.toggle('show')
+// bonus 
+    const ikona = document.querySelector(".fas")
+
+    if (polozky.classList.contains("show")) { 
+        ikona.classList.add("fa-xmark")
+    } else {
+        ikona.classList.remove("fa-xmark")
+    }
+}
+
+tlacitko.addEventListener('click', rozbalit)
