33f0f5416054fbec1a2f60752471dd43fbad299c
klaraplivova <126472015+klaraplivova@users.noreply.github.com>
2024-06-20 19:26:35 +0200

    formatted with prettier

--- spolecne.js
+++ spolecne.js
@@ -1,17 +1,17 @@
 // ukol 3
-const tlacitko = document.querySelector("#menu-tlacitko")
+const tlacitko = document.querySelector("#menu-tlacitko");
 
 const rozbalit = () => {
-    const polozky = document.querySelector('#menu-polozky')
-    polozky.classList.toggle('show')
-// bonus 
-    const ikona = document.querySelector(".fas")
+  const polozky = document.querySelector("#menu-polozky");
+  polozky.classList.toggle("show");
+  // bonus
+  const ikona = document.querySelector(".fas");
 
-    if (polozky.classList.contains("show")) { 
-        ikona.classList.add("fa-xmark")
-    } else {
-        ikona.classList.remove("fa-xmark")
-    }
-}
+  if (polozky.classList.contains("show")) {
+    ikona.classList.add("fa-xmark");
+  } else {
+    ikona.classList.remove("fa-xmark");
+  }
+};
 
-tlacitko.addEventListener('click', rozbalit)
+tlacitko.addEventListener("click", rozbalit);
